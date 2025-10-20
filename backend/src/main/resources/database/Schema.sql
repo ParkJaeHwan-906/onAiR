f669a8d187e6275849db8849cec32e41fff6d14b
ParkJaeHwan <doormoo2@naver.com>
2025-10-20 19:34:39 +0900

    Fix : Schema

--- backend/src/main/resources/database/Schema.sql
+++ backend/src/main/resources/database/Schema.sql
@@ -45,6 +45,7 @@ CREATE TABLE `user_accounts`(
 	`password` VARCHAR(255) NOT NULL COMMENT '계정 패스워드',
 	`role_id` BIGINT NOT NULL DEFAULT 2 COMMENT '계정의 권한 ( 기본은 사용자 )',
 	`online` TINYINT NOT NULL DEFAULT 0 COMMENT '온라인(출근) 여부 ( 0 : X, 1 : O )',
+	`equipment_id` BIGINT DEFAULT NULL COMMENT 'equipments 테이블의 id와 FK (주 담당 설비)',
 	`ban` TINYINT NOT NULL DEFAULT 0 COMMENT '계정의 ban 여부를 확인 ( 0 : X, 1 : O )',
 	`exit` DATE DEFAULT NULL COMMENT '계정의 탈퇴 여부를 확인',
 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -59,6 +60,27 @@ CREATE TABLE `user_accounts`(
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 ) COMMENT '사용자의 계정 정보를 저장';
+
+CREATE TABLE `refresh_token`(
+	`user_account_id` BIGINT NOT NULL,
+	`refresh_token` VARCHAR(255) NOT NULL,
+	`expired_at` DATETIME NOT NULL,
+	FOREIGN KEY(`user_account_id`) REFERENCES `user_accounts`(`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+) COMMENT '사용자의 리프레쉬 토큰 정보를 저장';
+
+CREATE TABLE attendance (
+     `user_account_id` BIGINT NOT NULL,
+     `work_date` DATE NOT NULL,
+     `check_in_time` DATETIME DEFAULT NULL,
+     `check_out_time` DATETIME DEFAULT NULL,
+     `total_work_minutes` INT DEFAULT NULL,
+     PRIMARY KEY (`user_account_id`, `work_date`),
+ 	 FOREIGN KEY (`user_account_id`) REFERENCES `user_accounts`(`id`)
+         ON DELETE CASCADE
+         ON UPDATE CASCADE
+ ) COMMENT '근로자들의 근태관리를 위한 테이블';
 
 -- 설비 정보 --
 CREATE TABLE `equipment_categories`(
@@ -110,35 +132,44 @@ CREATE TABLE `equipment_faq` (
 ) COMMENT '각 설비의 이전 작업 이력 및 메뉴얼 내용';
 
 -- 업무 --
-CREATE TABLE `task_bundles`(
-	`id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-	`company_id` BIGINT NOT NULL,
-	`code` VARCHAR(255) NOT NULL COMMENT '업무 코드',
-	`user_account_id` BIGINT NOT NULL,
-	`action` INT DEFAULT 1 COMMENT '0 : 취소, 1 : 진행 전, 2 : 진행중, 3 : 완료',
-	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	FOREIGN KEY(`company_id`) REFERENCES `companies`(`id`)
-		ON UPDATE CASCADE
-		ON DELETE CASCADE,
-	FOREIGN KEY(`user_account_id`) REFERENCES `user_accounts`(`id`)
-		ON UPDATE CASCADE
-		ON DELETE CASCADE
-) COMMENT '업무 목록을 저장하는 테이블';
+-- CREATE TABLE `task_bundles`(
+-- 	`id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+-- 	`company_id` BIGINT NOT NULL,
+-- 	`code` VARCHAR(255) NOT NULL COMMENT '업무 코드',
+-- 	`user_account_id` BIGINT NOT NULL,
+-- 	`action` INT DEFAULT 1 COMMENT '0 : 취소, 1 : 진행 전, 2 : 진행중, 3 : 완료',
+-- 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+-- 	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+-- 	FOREIGN KEY(`company_id`) REFERENCES `companies`(`id`)
+-- 		ON UPDATE CASCADE
+-- 		ON DELETE CASCADE,
+-- 	FOREIGN KEY(`user_account_id`) REFERENCES `user_accounts`(`id`)
+-- 		ON UPDATE CASCADE
+-- 		ON DELETE CASCADE
+-- ) COMMENT '업무 목록을 저장하는 테이블';
+
 
 CREATE TABLE `tasks`(
 	`id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-	`task_bundle_id` BIGINT NOT NULL,
+--	`task_bundle_id` BIGINT NOT NULL,
 	`equipment_id` BIGINT NOT NULL,
-	`address` TEXT NOT NULL COMMENT '업무를 진행할 위치 (도로명 주소)',
+	`company_id` BIGINT NOT NULL,
+	`user_account_id`BIGINT NOT NULL,
+--	`address` TEXT NOT NULL COMMENT '업무를 진행할 위치 (도로명 주소)',
 	`request` TEXT NOT NULL COMMENT '업무 요청사항',
 	`action` INT DEFAULT 1 COMMENT '0 : 취소, 1 : 진행 전, 2 : 진행중, 3 : 완료',
 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	FOREIGN KEY(`task_bundle_id`) REFERENCES `task_bundles`(`id`)
+--	FOREIGN KEY(`task_bundle_id`) REFERENCES `task_bundles`(`id`)
+--		ON UPDATE CASCADE
+--		ON DELETE CASCADE,
+	FOREIGN KEY(`equipment_id`) REFERENCES `equipments`(`id`)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
-	FOREIGN KEY(`equipment_id`) REFERENCES `equipments`(`id`)
+	FOREIGN KEY(`company_id`) REFERENCES `companies`(`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	FOREIGN KEY(`user_account_id`) REFERENCES `user_accounts`(`id`)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 ) COMMENT '업무 목록을 저장하는 테이블';
