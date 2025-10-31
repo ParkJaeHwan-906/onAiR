e2c54c271e788bbbfa81b8ec669627032fb8fe14
손동현 <as8542994@naver.com>
2025-10-31 10:53:12 +0900

    feat/Home 컴포넌트 완성

--- frontend/src/components/Home/HomeBig.tsx
+++ frontend/src/components/Home/HomeBig.tsx
@@ -2,11 +2,16 @@ import '../../styles/HomeBig.css';
 import WorkList from './WorkList';
 
 const dummy = [
-  {id: 1, userName: '손동현', solution: '네트워크 장비 교체', action: 2, actionStatus: '작업중'},
-  {id: 2, userName: '최선우', solution: '개발 환경 구성', action: 2, actionStatus: '작업중'},
-  {id: 3, userName: '박재환', solution: '커피 타기', action: 3, actionStatus: '완료'},
-  {id: 4, userName: '김준혁', solution: '데이터 백업', action: 1, actionStatus: '대기'},
-  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소'},
+  {id: 1, userName: '손동현', solution: '네트워크 장비 교체', action: 2, actionStatus: '작업중', lastUpdateTime: '19:08:01'},
+  {id: 2, userName: '최선우', solution: '개발 환경 구성', action: 2, actionStatus: '작업중', lastUpdateTime: '19:08:01'},
+  {id: 3, userName: '박재환', solution: '커피 타기', action: 3, actionStatus: '완료', lastUpdateTime: '19:08:01'},
+  {id: 4, userName: '김준혁', solution: '데이터 백업', action: 1, actionStatus: '대기', lastUpdateTime: '19:08:01'},
+  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소', lastUpdateTime: '19:08:01'},
+  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소', lastUpdateTime: '19:08:01'},
+  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소', lastUpdateTime: '19:08:01'},
+  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소', lastUpdateTime: '19:08:01'},
+  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소', lastUpdateTime: '19:08:01'},
+  {id: 5, userName: '김나영', solution: '서버 점검', action: 0, actionStatus: '취소', lastUpdateTime: '19:08:01'},
 ]
 
 type HomeBigProps = {
@@ -31,16 +36,22 @@ function HomeBig({ title, icon }: HomeBigProps) {
         <div className='third'>
           상태
         </div>
+        <div className='fourth'>
+          변경시간
+        </div>
       </div>
-      {dummy.map((item) => (
-        <WorkList 
-          key={item.id}
-          userName={item.userName}
-          solution={item.solution}
-          action={item.action}
-          actionStatus={item.actionStatus}
-        />
-      ))}
+      <div className='list-body'>
+        {dummy.map((item) => (
+          <WorkList 
+            key={item.id}
+            userName={item.userName}
+            solution={item.solution}
+            action={item.action}
+            actionStatus={item.actionStatus}
+            lastUpdateTime={item.lastUpdateTime}
+          />
+        ))}
+      </div>
     </div>
   );
 };
