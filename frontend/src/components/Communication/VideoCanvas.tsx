b0174856c8b75eba5542f81ebe0f239e97f901d1
김준혁 <bears081887@gmail.com>
2025-11-05 21:53:37 +0900

    feat: communication 페이지 - 수신받고있는 영상 렌더링

--- frontend/src/components/Communication/VideoCanvas.tsx
+++ frontend/src/components/Communication/VideoCanvas.tsx
@@ -1,6 +1,8 @@
 // import { VideoTrack } from "@livekit/components-react"
 import type { DrawingLine } from "../../types/DrawingLine";
 import { OverlayCanvas } from "./OverlayCanvas";
+import { useSocket } from "../../utils/socketContext";
+import { useEffect, useRef, useState } from "react";
 
 interface VideoProps {
   handleSerialize: (lines: DrawingLine[]) => void;
@@ -12,6 +14,33 @@ export const VideoCanvas = ({
   penColor,
   currentTool,
 }: VideoProps) => {
+  const socket = useSocket();   // 연결되어있는 소켓 객체를 가져옴
+  const videoCanvasRef = useRef<HTMLCanvasElement>(null);
+  const imgRef = useRef<HTMLImageElement>(new Image());
+  const [isConnected, setIsConnected] = useState(false); // 첫 프레임 수신 여부
+
+
+  useEffect(() => {
+    const canvas = videoCanvasRef.current;
+    const ctx = canvas?.getContext('2d');
+    const img = imgRef.current;
+    if (!canvas || !ctx) return;
+
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      if (!isConnected) setIsConnected(true);
+    };
+
+    // 서버로부터 video_frame 이벤트 수신
+    socket.on('video_frame', (data) => {
+      img.src = `data:image/jpeg;base64,${data.frame}`;
+    });
+
+    return () => {
+      socket.off('video_frame'); // clean up
+    };
+  }, [socket]);
+
   return (
     <div
       style={{
@@ -34,7 +63,24 @@ export const VideoCanvas = ({
         }}
       >
         {/* <VideoTrack /> */}
-        <p style={{ color: "white" }}>(Video)</p>
+        {/* <p style={{ color: "white" }}>(Video)</p> */}
+        <canvas 
+          ref={videoCanvasRef}
+          width={940}
+          height={857}
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+        {!isConnected && (
+          <p style={{ 
+            color: 'white',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}>
+            연결 대기중...
+          </p>
+        )}
       </div>
       <div
         style={{
