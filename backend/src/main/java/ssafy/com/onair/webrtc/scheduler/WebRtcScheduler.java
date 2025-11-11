1a1dd8bfe0e6fb9ac2633f3063aa955aa7d7314e
ParkJaeHwan <doormoo2@naver.com>
2025-11-11 19:00:22 +0900

    Fix SSE Event

---
+++ backend/src/main/java/ssafy/com/onair/webrtc/scheduler/WebRtcScheduler.java
@@ -0,0 +1,34 @@
+package ssafy.com.onair.webrtc.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ssafy.com.onair.sse.manager.SseManager;
+import ssafy.com.onair.webrtc.manager.WebRtcManager;
+
+import java.time.LocalDateTime;
+import java.util.StringTokenizer;
+
+@Component
+@RequiredArgsConstructor
+public class WebRtcScheduler {
+    private final WebRtcManager webRtcManager;
+    private final SseManager sseManager;
+
+//    @Scheduled(fixedRate = 1000 * 60 * 5)
+    @Scheduled(fixedRate = 1000 * 20)
+    public void removeTimeOutRooms() {
+        if(webRtcManager.getWaitingRoomList().isEmpty()) return;
+
+        String[] roomNameList = webRtcManager.getWaitingRoomList().keySet().toArray(new String[0]);
+        for(String roomName : roomNameList) {
+            if(webRtcManager.getWaitingRoomList().get(roomName).isAfter(LocalDateTime.now())) {
+                webRtcManager.getWaitingRoomList().remove(roomName);
+                StringTokenizer st = new StringTokenizer(roomName);
+                Long senderAccountId = Long.parseLong(st.nextToken());
+                Long receiverAccountId = Long.parseLong(st.nextToken());
+                sseManager.sendRtcCancelEvent(senderAccountId, receiverAccountId);
+            }
+        }
+    }
+}
