5a0f964d6f9d59a0eb70bd06292a94bd3bb0368b
Naling1225 <1225julia@naver.com>
2025-11-16 19:59:08 +0900

    fix: ar-info 복구, color 형식 변경

--- mobile/app/src/main/java/com/onair/mobile/communicate/data/socket/dto/ArMarkerResponse.kt
+++ mobile/app/src/main/java/com/onair/mobile/communicate/data/socket/dto/ArMarkerResponse.kt
@@ -12,7 +12,7 @@ data class ArMarker(
     val type: String,
     val idx: Int,
     val info: MarkerInfo,
-    val color : String?,
+    val color : String,
     val pulseScale: Float,
     val pulseOpacity: Float,
     val opacity: Float
