69a1968d9172b979c225191baef1b7f9cea80859
DESKTOP-RBNO6LS\SSAFY <lbh3939@gmail.com>
2025-11-18 15:24:49 +0900

    feat: 삭제된 시작 로직 추가

--- ai_server/yolo_service/app/main.py
+++ ai_server/yolo_service/app/main.py
@@ -3,7 +3,6 @@ import asyncio
 from fastapi import FastAPI
 from loguru import logger
 from fastapi import APIRouter
-from ai_server.yolo_service.device_detector import start_device_detector
 from ai_server.yolo_service.redis_client import get_device_state
 from ai_server.yolo_service.anomaly import run_anomaly_detection
 
