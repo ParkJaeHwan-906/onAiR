cdec7e8537eec27ed7382f59fcedd55ff1483128
sojung0734 <sojung0734@naver.com>
2025-11-20 04:30:42 +0900

    fix:오류 수정

--- ai_server/yolo_service/anomaly.py
+++ ai_server/yolo_service/anomaly.py
@@ -124,7 +124,7 @@ async def run_anomaly_detection():
 
     # 6) anomaly 존재 여부
     has_anomaly = any(
-        v.get("status") in ("anomaly", "warning")
+        v.get("status") != ("not_found")
         for v in anomalies.values()
     )
     # 7) 최종 반환
